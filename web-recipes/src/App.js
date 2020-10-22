79b99ab48fc582c9c06e841d1863551cfa0ccf3d
Miguel Angel Borda <mborda609@hotmail.com>
2020-10-22 18:11:19 -0300

    Commit inicial Commit de react app web

---
+++ web-recipes/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './Components/Grid'
+import Grid from './Components/Grid';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">|
+        <Grid />
+      </div>
+    );
+  }
+}
+
+export default App;
